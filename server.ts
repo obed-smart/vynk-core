fc6de359616863f7596206afd4edac9a15181d5f
Obed✨✨ <135765970+obed-smart@users.noreply.github.com>
2025-11-08 18:17:47 +0100

    Update server log message to include 'port:'

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ async function startServer() {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+      console.log(`🚀 Server running on port: ${PORT}`);
     });
   } catch (error: unknown) {
     if (error instanceof Error) {
